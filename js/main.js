e27d1be4dacf499c530c9517f3917c1ef5ca8ef6
susumu <cvwwvc102@gmail.com>
2018-02-27 18:00:18 +0900

    main.jsのsmoothscrollの記述を変更

--- js/main.js
+++ js/main.js
@@ -1,11 +1,39 @@
-    // smoothscroll
-    // #にダブルクォーテーションが必要
-    $('a[href^="#smooth1"]').click(function() {
-       var headerHight = 100;
-       var speed = 800;
-       var href= $(this).attr("href");
-       var target = $(href == "#" || href == "" ? 'html' : href);
-       var position = target.offset().top-headerHight;
-       $('body,html').animate({scrollTop:position}, speed, 'swing');
-       return false;
-    });
+/* Copyright(C) 2009 web design lab (http://www.webdlab.com/) Licensed under the MIT License http://www.opensource.org/licenses/mit-license.php */
+/*------------------------------------------------------------------*/
+function time(){
+if(Math.abs(current_y - target_y)<Math.abs(speed)){
+window.scrollTo((document.body.scrollTop || document.documentElement.scrollTop),target_y);
+clearInterval(timer);
+return false;
+}else{
+window.scrollBy(0,speed);
+current_y += speed;
+}
+}
+function move(val){
+speed = 50;
+smooth = 25;
+current_y = document.body.scrollTop || document.documentElement.scrollTop;
+path = '' + val;
+target = path.split('#');
+element = document.getElementById(target[1]);
+target_y = 0;
+for(i = element;i.offsetParent;i = i.offsetParent){
+target_y += i.offsetTop;
+}
+if(current_y > target_y){
+speed = -(speed);
+}
+timer = setInterval('time()',smooth);
+return false;
+}
+function act(){
+var a = document.querySelectorAll('a[href^="#"]');
+for(var i=0; i < a.length; i++){
+a[i].onclick = function(){
+move(this);
+return false;
+}
+}
+}
+window.addEventListener('DOMContentLoaded',act,false);
