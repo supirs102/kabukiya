3c7b4f03d71b4c03fea8346ef1e0e2b516178066
吉岡じゅえり <supirs102@gmail.com>
2018-02-27 18:22:16 +0900

    ごよやくのページシステムとりあえず埋め込み 確認のページが緑の上に出たり問題多発中

--- reservation.php
+++ reservation.php
@@ -7,6 +7,7 @@
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 <title>ごよやく&emsp;|&emsp;歌舞伎屋</title>
 <link rel="stylesheet" href="css/style.css" type="text/css">
+<link href="calendar_form/style.css" rel="stylesheet">
 <script src="js/main.js" type="text/javascript"></script>
 <link rel="icon" type="image/vnd.microsoft.icon" href="#">
 </head>
@@ -41,6 +42,72 @@
 		生態系に恵まれている<br>
 		場所であることに気づく</p>
 	</div><!-- /reserveabout -->
+	<div id="reservesystem">
+		<?php require_once('calendar_form/admin/config.php'); ?>
+		<?php if(!isset($_POST["reservSubmit"]) && empty($_GET['mode'])){ ?>
+		<p class="holidayText"><?php if($closedText) echo $closedText; ?><span class="holidayCube" style="background:<?php echo $holidayBg ;?>"></span>休業日</p>
+		<?php if(!$copyright) exit($warningMesse); else{ echo scheduleCalenderPc($ym,$timeStamp,$copyright); ?>
+		<p class="holidayText"><?php if($closedText) echo $closedText; ?><span class="holidayCube" style="background:<?php echo $holidayBg ;?>"></span>休業日</p>
+		<?php Uqa4h78r();} ?>
+		<?php }else{ ?>
+		<?php
+		$date = (isset($_GET["date"])) ? calf_h($_GET["date"]) : exit('日付が選択されていません。戻って選択しなおして下さい<br /><a href="javascript:history.back()">戻る&raquo;<a>');
+		$time = (isset($_GET["time"])) ? calf_h($_GET["time"]) : '';
+		$dateArray = explode("-",$date);
+		$dspDate = $dateArray[0]."年".$dateArray[1]."月".$dateArray[2]."日";
+		$dspDate .= ($weekDsp == 1) ? '（'.$weekArray[date('w',strtotime($date))].'）' : '';
+		$dspDate .= " ".$timeArray[$time];
+		?>
+
+		<div id="formWrap">
+		<p><input type="button" value="前画面に戻る" onClick="history.back()" class="submit-button"></p>
+			<h2>予約フォーム</h2>
+			<p>下記フォームに必要事項を入力後、確認ボタンを押してください。</p>
+		<form action="calendar_form/mail/mail.php" method="post">
+		<table class="formTable">
+			<tr>
+				<th><?php echo $selectDateText;?></th>
+				<td>
+			<?php echo $dspDate;?> <input type="hidden" name="reserv[date]" value="<?php echo $date;?>" /><input type="hidden" name="reserv[time]" value="<?php echo $time;?>">
+				</td>
+			</tr>
+			<tr>
+				<th>お名前</th>
+				<td><input size="20" type="text" name="お名前">※必須</td>
+			</tr>
+			<tr>
+				<th>電話番号（半角）</th>
+				<td><input size="30" type="text" name="電話番号"></td>
+			</tr>
+			<tr>
+				<th>メールアドレス（半角）</th>
+				<td><input size="30" type="text" name="メールアドレス">※必須</td>
+			</tr>
+			<tr>
+				<th>性別</th>
+				<td><input type="radio" name="性別" value="男">&emsp;男&emsp;
+					<input type="radio" name="性別" value="女">&emsp;女&emsp;
+				</td>
+			</tr>
+			<tr>
+				<th>サイトを知ったきっかけ</th>
+				<td><input name="サイトを知ったきっかけ[]" type="checkbox" value="友人・知人">友人・知人
+					<input name="サイトを知ったきっかけ[]" type="checkbox" value="検索エンジン">検索エンジン
+				</td>
+			</tr>
+			<tr>
+				<th>お問い合わせ内容</th>
+				<td><textarea name="お問い合わせ内容" cols="50" rows="5"></textarea></td>
+			</tr>
+		</table>
+		<p align="center">
+			<input type="submit" value="&emsp;&emsp;確認&emsp;&emsp;">
+			<input type="reset" value="リセット">
+		</p>
+		</form>
+		</div>
+		<?php Uqa4h78r();} ?>
+	</div><!-- /reservesystem -->
 </div><!-- /reservationmain -->
 <footer>
 	<div id="footerdata">
