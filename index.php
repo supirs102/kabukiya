4c3cfc0148140b989b1ab5d47929ea85ec6cada3
susumu <cvwwvc102@gmail.com>
2018-02-27 19:12:28 +0900

    jqaryの読み込みを記述

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
 <title>歌舞伎屋</title>
 <link rel="stylesheet" href="css/style.css" type="text/css">
-<script src="js/main.js" type="text/javascript"></script>
+<script src="js/main.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <link rel="icon" type="image/vnd.microsoft.icon" href="#">
 </head>
 <body>
