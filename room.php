27a5010ba62ffef271932ad7ba6852832e7c994f
susumu <cvwwvc102@gmail.com>
2018-03-02 00:30:47 +0900

    左のhoverの画像の変更 h2のサイズ変更

--- room.php
+++ room.php
@@ -143,7 +143,7 @@
 	<img class="bg11" src="images/goyoyaku_top.png">
 	<img class="bg12" src="images/goannai_top.png">
 	<img class="bg13" src="images/ofuro_top.png">
-	<img class="bg14" src="images/osyokuji.png">
+	<img class="bg14" src="images/osyokuji_top.jpg">
 	<img class="bg15" src="images/goemon_top.jpg">
 	<img class="bg16" src="images/top_face.png">
 </div><!-- /bgmain -->
